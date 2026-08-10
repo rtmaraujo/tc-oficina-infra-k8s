01601d84ea8541d90b717112b9639793ce9e6f19
Marcos Araujo <rtmaraujo@gmail.com>
2026-08-10 20:36:19 -0300

    style: terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,7 +103,7 @@ resource "aws_eks_cluster" "tc" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids         = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
+    subnet_ids             = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
     endpoint_public_access = true
   }
 
